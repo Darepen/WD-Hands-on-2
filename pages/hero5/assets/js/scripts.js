df0e2d7d907b42d349089683ce2f30c94effe67a
Darepen <juliojosepedrena@gmail.com>
2024-06-22 20:49:08 +0800

    removed footersw

--- pages/hero5/assets/js/scripts.js
+++ pages/hero5/assets/js/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     var audio = document.getElementById('background-audio');
     if (audio) {
-        audio.volume = 0.15;
+        audio.volume = 0.45;
     } else {
         console.error("Audio element not found");
     }
